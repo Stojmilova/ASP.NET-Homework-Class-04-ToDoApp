e8962b6efe47999ef4221fbbf1fd9077023d724a
Cvetanka <45522819+Stojmilova@users.noreply.github.com>
2019-06-09 21:46:54 +0200

    Initial commit

---
+++ ToDoTasks/Models/DomainModels/SubTask.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoTasks.Models.DomainModels
+{
+    public class SubTask
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public TypeStatusSubTask Status { get; set; }
+        
+    }
+    public enum TypeStatusSubTask
+    {
+        Done,
+        NotDone,
+    }
+}
---
+++ ToDoTasks/Models/DomainModels/ToDoTask.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoTasks.Models.DomainModels
+{
+    public class ToDoTask
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public TypePriority Priority { get; set; }  
+        public TypeStatus Status { get; set; }
+        public TypeOfTask TaskType { get; set; }
+        public List<SubTask> SubTasks { get; set; }
+
+        public ToDoTask()
+        {
+            SubTasks = new List<SubTask>();
+        }
+    }
+
+    public enum TypePriority
+    {
+        Important = 1,
+        MediumImportance,
+        NotImportant,
+
+    }
+    public enum TypeStatus
+    {
+        NotDone,
+        InProgress,
+        Done
+    }
+    public enum TypeOfTask
+    {
+        Work,
+        Personal,
+        Hobby
+    }
+}
